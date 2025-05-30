ce3f10870760817b57da8e6638314a4aafe92f1a
Shirsendu-Schrodinger <shirsendu.pal@schrodinger.com>
2025-05-30 18:06:24 +0530

    Added few more fields to the Task class

--- stv_demo.py
+++ stv_demo.py
@@ -8,16 +8,17 @@ from dataclasses import dataclass
 from typing import List
 from textual.reactive import reactive
 from textual import events
+from datetime import datetime
 
 
 @dataclass
 class TaskInfo:
     id: int
     name: str
-    # start_time: datetime
+    start_time: datetime
     status: str
-    # logs: List[str]
-    # description: str
+    logs: List[str]
+    description: str
 
 
 class Task(Widget):
@@ -59,18 +60,15 @@ class TaskList(Widget):
 
     def load_data(self):
         return [
-            TaskInfo(id=102, name="Task1", status="Running"),
-            TaskInfo(id=106, name="Task2", status="Failed"),
-            TaskInfo(id=204, name="Task3", status="Success"),
-            TaskInfo(id=105, name="Task4", status="Failed"),
-            TaskInfo(id=202, name="Task5", status="Waiting"),
-            TaskInfo(id=109, name="Task6", status="Running"),
-            TaskInfo(id=201, name="Task7", status="Failed"),
-            TaskInfo(id=100, name="Task8", status="Success")
+            TaskInfo(id=102, name="Task1", start_time=datetime.strptime("29-05-25 14:07:20.46",'%d-%m-%y %H:%M:%S.%f'), status="Running", logs=["Some Process\n"], description="Created by User A"),
+            TaskInfo(id=106, name="Task2", status="Failed", start_time=datetime.strptime("12-03-24 09:15:32.74",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n", "Failed process\n"], description="Created by User X"),
+            TaskInfo(id=204, name="Task3", status="Success", start_time=datetime.strptime("28-11-22 22:41:08.19",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n", "Process completed\n"], description="Created by User B"),
+            TaskInfo(id=105, name="Task4", status="Failed", start_time=datetime.strptime("07-07-25 13:58:45.03",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n", "Failed process\n"], description="Created by User Y"),
+            TaskInfo(id=202, name="Task5", status="Waiting", start_time=datetime.strptime("16-01-23 04:27:11.89",'%d-%m-%y %H:%M:%S.%f'), logs=["Waiting in Queue\n"], description="Created by User A"),
+            TaskInfo(id=109, name="Task6", status="Running", start_time=datetime.strptime("03-06-24 17:36:55.62",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n"], description="Created by User L"),
+            TaskInfo(id=201, name="Task7", status="Failed", start_time=datetime.strptime("25-10-21 20:09:03.47",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n", "Failed process\n"], description="Created by User P"),
+            TaskInfo(id=100, name="Task8", status="Success", start_time=datetime.strptime("19-02-25 06:12:29.90",'%d-%m-%y %H:%M:%S.%f'), logs=["Some Process\n", "Process completed\n"], description="Created by User C")
         ]
-
-    # def on_task_selected(self, message: Task.Selected) -> None:
-    #     self.post_message(message)
 
 
 class TaskDetails(Static):
@@ -79,20 +77,19 @@ class TaskDetails(Static):
     def __init__(self, id):
         super().__init__(id=id)
 
-    # def compose(self) -> ComposeResult:
-        # yield Label("Select a task to view details")
-
     def compose(self) -> ComposeResult:
         self.label = Label("Select a task to view details", id="details-label")
         with VerticalScroll():
             yield self.label
 
     def update_task(self, task_info: TaskInfo) -> None:
-        self.label.update(
-            f"[b]Task ID:[/b] {task_info.id}\n"
-            f"[b]Name:[/b] {task_info.name}\n"
-            f"[b]Status:[/b] {task_info.status}"
-        )
+        self.label.update(f"[b]Task ID:[/b] {task_info.id}\n"
+                          f"[b]Name:[/b] {task_info.name}\n"
+                          f"[b]Status:[/b] {task_info.status}\n"
+                          f"[b]Creation Time:[/b] {task_info.start_time}\n"
+                          f"[b]Description:[/b] {task_info.description}\n\n"
+                          f"{'-'*10 + ' [b]Logs[/b] ' + '-'*10}\n"
+                          f"{''.join([line for line in task_info.logs])}")
 
 
 class TaskViewerApp(App):
